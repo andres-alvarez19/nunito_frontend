95157b6e1794eb314b30c437eaa595f7a93d67de
Andres Alvarez <a.alvarez08@ufromail.cl>
2025-11-28 08:31:21 -0300

    Api funciona telefono

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    BASE_URL: "http://nunito-backend-production.up.railway.app/api",
+    BASE_URL: "https://nunito-backend-production.up.railway.app/api",
     TIMEOUT: 10000,
 };
